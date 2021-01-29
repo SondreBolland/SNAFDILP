ac8f30261e6fbb412d83500a98d87b331df387af
Sondre.Bolland1 <sondre.bolland@student.uib.no>
2021-01-29 16:20:36 +0100

    Altered Rule_Template_Negation to have all code from Rule_Template, instead of inheriting from it

--- src/ilp/template/rule_Template_Negation.py
+++ src/ilp/template/rule_Template_Negation.py
@@ -1,17 +1,28 @@
-from src.ilp import Rule_Template
 '''Defines the extended rule template including a parameter for negation
 '''
 
 
-class Rule_Template_Negation(Rule_Template):
+class Rule_Template_Negation:
 
-    def __init__(self, neg: bool):
+    def __init__(self, v: int, allow_intensional: bool, neg: bool):
         '''
         Arguments:
+            v {int} -- numberof existentially quantified variable allowed in the clause
+            allow_intensional {bool} -- True is intensional predicates are allowed, False if only extensional predicates
             neg {bool} -- True if negative literals are allowed, False if only positive literals are allowed
         '''
 
+        self._v = v
+        self._allow_intensional = allow_intensional
         self._neg = neg
+
+    @property
+    def v(self):
+        return self._v
+
+    @property
+    def allow_intensional(self):
+        return self._allow_intensional
 
     @property
     def neg(self):
